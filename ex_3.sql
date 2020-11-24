6b0ef6edcd7fb1d87b2af2c22dad3ac2d61f515a
Filipp <filipp-z.mail.ru>
2020-11-24 09:56:40 +0300

    ex_3 update

--- ex_3.sql
+++ ex_3.sql
@@ -6,12 +6,11 @@ create table executors(
 create table albums(
     id serial primary key,
     name varchar(60) not null,
-    year integer,
-    executor_id integer references executors(id)
+    year integer
 );
 
 create table trecks(
-    id serial primary key,
+    id integer primary key,
     name varchar(60) not null,
     duration numeric,
     album_id integer references albums(id)
@@ -19,8 +18,7 @@ create table trecks(
 
 create table genres(
     id serial primary key,
-    name varchar(60) not null,
-    executor_id integer references executors(id)
+    name varchar(60) not null
 );
 
 create table executors_genres(
@@ -36,16 +34,20 @@ create table executors_album(
 );
 
 create table collections(
-    id serial primary key,
+    id integer primary key,
     name varchar(60) not null,
-    year integer,
-    treck_id integer references trecks(id),
-    album_id integer references albums(id)
+    year integer
 );
 
 create table employees(
-    id serial primary key,
+    id integer primary key,
     name varchar(60) not null,
     department varchar(60),
-    chief varchar(60)
+    chief integer references employees(id)
+);
+
+create table collections_trecks(
+    id serial primary key,
+    treck_id integer references trecks(id),
+    collection_id integer references collections(id)
 );
