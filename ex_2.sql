ffc6554fdc6d741a6132dbdc06d9b07500122b6e
Filipp <filipp-z.mail.ru>
2020-11-24 09:30:20 +0300

    ex_2 update

--- ex_2.sql
+++ ex_2.sql
@@ -4,26 +4,26 @@ create database music_site with owner filipp;
 
 create table executors(
     id serial primary key,
-    name varchar(60)
+    name varchar(60),
+    genres_id integer references genres(id)
 );
 
 create table albums(
     id serial primary key,
     name varchar(60) not null,
     year integer,
-    executor_id serial
+    executor_id integer references executors(id)
 );
 
 create table trecs(
     id serial primary key,
     name varchar(60) not null,
     duration numeric,
-    album_id serial
+    album_id integer references albums(id)
 );
 
 create table genres(
     id serial primary key,
-    name varchar(60) not null,
-    executor_id serial
+    name varchar(60) not null
 );
 
